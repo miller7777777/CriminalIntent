21cf51539cdc064f4e70f18dac31189a417a3f39
miller777 <miller777@mail.ru>
2018-01-30 02:22:06 +0300

    DatePicker set first day of week;

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -51,6 +52,10 @@ public class DatePickerFragment extends DialogFragment {
 
         mDatePicker = (DatePicker) w.findViewById(R.id.dialog_date_picker);
         mDatePicker.init(year, month, day, null);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mDatePicker.setFirstDayOfWeek(Calendar.MONDAY);
+        }
+
 
         return new AlertDialog.Builder(getActivity())
                 .setView(w)
